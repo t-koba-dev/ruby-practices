48fc21b7df9ff927343412d79a76656b02beaeac
ToshiroKoba <t-koba@ruby-dev.jp>
2021-03-17 16:39:14 +0900

    Gameクラスのscoreメソッド定義に伴いテストを修正

--- 07.bowling_object/bowling_oop_test.rb
+++ 07.bowling_object/bowling_oop_test.rb
@@ -7,26 +7,26 @@ require './07.bowling_object/bowling_oop'
 class BowlingOopTest < Minitest::Test
   def test_case1
     scores = '6,3,9,0,0,3,8,2,7,3,X,9,1,8,0,X,6,4,5'.split(',')
-    assert_equal 139, calculate(scores)
+    assert_equal 139, Game.new(scores).score
   end
 
   def test_case2
     scores = '6,3,9,0,0,3,8,2,7,3,X,9,1,8,0,X,X,X,X'.split(',')
-    assert_equal 164, calculate(scores)
+    assert_equal 164, Game.new(scores).score
   end
 
   def test_case3
     scores = '0,10,1,5,0,0,0,0,X,X,X,5,1,8,1,0,4'.split(',')
-    assert_equal 107, calculate(scores)
+    assert_equal 107, Game.new(scores).score
   end
 
   def test_case4
     scores = '6,3,9,0,0,3,8,2,7,3,X,9,1,8,0,X,X,0,0'.split(',')
-    assert_equal 134, calculate(scores)
+    assert_equal 134, Game.new(scores).score
   end
 
   def test_case5
     scores = 'X,X,X,X,X,X,X,X,X,X,X,X'.split(',')
-    assert_equal 300, calculate(scores)
+    assert_equal 300, Game.new(scores).score
   end
 end
