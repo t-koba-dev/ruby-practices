ab4f7360705f7b9beda3193dca61306a2bf28688
ToshiroKoba <t-koba@ruby-dev.jp>
2021-03-17 16:29:39 +0900

    整形処理の切り出しに伴いテストを修正

--- 07.bowling_object/bowling_oop_test.rb
+++ 07.bowling_object/bowling_oop_test.rb
@@ -6,27 +6,27 @@ require './07.bowling_object/bowling_oop'
 
 class BowlingOopTest < Minitest::Test
   def test_case1
-    score = '6,3,9,0,0,3,8,2,7,3,X,9,1,8,0,X,6,4,5'
-    assert_equal 139, calculate(score)
+    scores = '6,3,9,0,0,3,8,2,7,3,X,9,1,8,0,X,6,4,5'.split(',')
+    assert_equal 139, calculate(scores)
   end
 
   def test_case2
-    score = '6,3,9,0,0,3,8,2,7,3,X,9,1,8,0,X,X,X,X'
-    assert_equal 164, calculate(score)
+    scores = '6,3,9,0,0,3,8,2,7,3,X,9,1,8,0,X,X,X,X'.split(',')
+    assert_equal 164, calculate(scores)
   end
 
   def test_case3
-    score = '0,10,1,5,0,0,0,0,X,X,X,5,1,8,1,0,4'
-    assert_equal 107, calculate(score)
+    scores = '0,10,1,5,0,0,0,0,X,X,X,5,1,8,1,0,4'.split(',')
+    assert_equal 107, calculate(scores)
   end
 
   def test_case4
-    score = '6,3,9,0,0,3,8,2,7,3,X,9,1,8,0,X,X,0,0'
-    assert_equal 134, calculate(score)
+    scores = '6,3,9,0,0,3,8,2,7,3,X,9,1,8,0,X,X,0,0'.split(',')
+    assert_equal 134, calculate(scores)
   end
 
   def test_case5
-    score = 'X,X,X,X,X,X,X,X,X,X,X,X'
-    assert_equal 300, calculate(score)
+    scores = 'X,X,X,X,X,X,X,X,X,X,X,X'.split(',')
+    assert_equal 300, calculate(scores)
   end
 end
