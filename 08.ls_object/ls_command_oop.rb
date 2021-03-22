db16dd06e7ad982bb74af5fc19c0b36fb9cc8087
ToshiroKoba <t-koba@ruby-dev.jp>
2021-03-22 11:56:43 +0900

    rubocopによる整形

--- 08.ls_object/ls_command_oop.rb
+++ 08.ls_object/ls_command_oop.rb
@@ -77,15 +77,13 @@ end
 class List
   def self.push_file_to_list
     file_list = []
-    regex = /.*\/+.*/
+    regex = %r{/.*/+.*/}
     Find.find('.') do |f|
       if f == '.'
         file_list << f
         next
       end
-      unless f.slice(2..-1).match?(regex)
-        file_list << f.slice(2..-1)
-      end
+      file_list << f.slice(2..-1) unless f.slice(2..-1).match?(regex)
     end
     Find.find('..') do |f|
       file_list << f
@@ -147,18 +145,18 @@ end
 
 class Calculation
   def self.calculate_word_max_length(result_list)
-    word_max_length = {}
-    word_max_length[:max_nlink] = Pathname(result_list.max_by { |file| Pathname(file).stat.nlink.to_s.size }).stat.nlink.to_s.size
-    word_max_length[:max_uid_name] = Etc.getpwuid(Pathname(result_list.max_by { |file| Etc.getpwuid(Pathname(file).stat.uid).name.to_s.size }).stat.uid).name.size
-    word_max_length[:max_gid_name] = Etc.getgrgid(Pathname(result_list.max_by { |file| Etc.getgrgid(Pathname(file).stat.gid).name.to_s.size }).stat.gid).name.size
-    word_max_length[:max_size] = Pathname(result_list.max_by { |file| Pathname(file).stat.size.to_s.size }).stat.size.to_s.size
-    word_max_length
+    {
+      max_nlink: Pathname(result_list.max_by { |file| Pathname(file).stat.nlink.to_s.size }).stat.nlink.to_s.size,
+      max_uid_name: Etc.getpwuid(Pathname(result_list.max_by { |file| Etc.getpwuid(Pathname(file).stat.uid).name.to_s.size }).stat.uid).name.size,
+      max_gid_name: Etc.getgrgid(Pathname(result_list.max_by { |file| Etc.getgrgid(Pathname(file).stat.gid).name.to_s.size }).stat.gid).name.size,
+      max_size: Pathname(result_list.max_by { |file| Pathname(file).stat.size.to_s.size }).stat.size.to_s.size
+    }
   end
 
   def self.calculate_total(result_list)
     enqueue_file = []
     result_list.each { |f| enqueue_file << f if /^[^.]/.match?(f) }
-    file_count = enqueue_file.inject(0) { |result, f| result + 8 * ((Pathname(f).stat.size)/4096 + 1 ) }
+    enqueue_file.inject(0) { |result, f| result + 8 * (Pathname(f).stat.size / 4096 + 1) }
   end
 end
 
