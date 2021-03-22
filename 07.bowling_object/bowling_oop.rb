690e414758a128622e018ed3bc0d63408498612b
ToshiroKoba <t-koba@ruby-dev.jp>
2021-03-22 13:50:17 +0900

    コマンドオプションの前処理をOptionクラスに切り出し

--- 07.bowling_object/bowling_oop.rb
+++ 07.bowling_object/bowling_oop.rb
@@ -3,7 +3,8 @@
 require './07.bowling_object/game'
 require './07.bowling_object/frame'
 require './07.bowling_object/shot'
+require './07.bowling_object/argv'
 
-scores = ARGV[0]&.split(',')
+scores = Option.enqueue
 
 p Game.new(scores).score
