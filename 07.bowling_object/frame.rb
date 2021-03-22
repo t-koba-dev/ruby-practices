6ded9f6c59f72c7b3bfcd378d5b73d7d6e23c107
ToshiroKoba <t-koba@ruby-dev.jp>
2021-03-22 15:45:01 +0900

    フレームに投球スコアを格納する処理をGameクラスのプライベートなメソッドとして切り出し

--- 07.bowling_object/frame.rb
+++ 07.bowling_object/frame.rb
@@ -2,46 +2,11 @@
 
 class Frame
   attr_reader :first_shot, :second_shot, :third_shot
-  THE_PREVIOUS_FRAME_FROM_THE_END = 9
-  MAXIMUM_NUMBER_OF_SHOTS_IN_THE_FRAME = 2
 
   def initialize(first_mark, second_mark, third_mark = nil)
     @first_shot = Shot.new(first_mark)
     @second_shot = Shot.new(second_mark)
     @third_shot = Shot.new(third_mark)
-  end
-
-  def self.push_shots_to_frames(scores)
-    frames = []
-    shots = []
-    scores.each_with_index do |shot, index|
-      number_of_shots = index + 1
-      if frames.size == THE_PREVIOUS_FRAME_FROM_THE_END
-        push_when_frame10(scores, number_of_shots, shots)
-        frames.push(Frame.new(shots[0], shots[1], shots[2]))
-        break
-      else
-        push_when_others(shot, shots, frames)
-        if shots.size == MAXIMUM_NUMBER_OF_SHOTS_IN_THE_FRAME
-          frames.push(Frame.new(shots[0], shots[1]))
-          shots.clear
-        end
-      end
-    end
-
-    frames
-  end
-
-  def self.push_when_frame10(scores, number_of_shots, shots)
-    scores[(number_of_shots - 1)..-1].each { |i| shots << Shot.new(i).score }
-  end
-
-  def self.push_when_others(shot, shots, frames)
-    if shots.size.zero? && (shot == 'X') # strike
-      frames.push(Frame.new(10, 0))
-    else
-      shots << Shot.new(shot).score
-    end
   end
 
   def score(frames)
--- 07.bowling_object/game.rb
+++ 07.bowling_object/game.rb
@@ -3,16 +3,54 @@
 class Game
   attr_reader :scores
 
+  THE_PREVIOUS_FRAME_FROM_THE_END = 9
+  MAXIMUM_NUMBER_OF_SHOTS_IN_THE_FRAME = 2
+
   def initialize(scores)
     @scores = scores
   end
 
   def score
-    frames = Frame.push_shots_to_frames(scores)
+    frames = push_shots_to_frames(scores)
     point = 0
     9.times do |frame|
       point += frames[frame].score(frames)
     end
     point += frames.last.score_when_last_frame
+  end
+
+  private
+
+  def push_shots_to_frames(scores)
+    frames = []
+    shots = []
+    scores.each_with_index do |shot, index|
+      number_of_shots = index + 1
+      if frames.size == THE_PREVIOUS_FRAME_FROM_THE_END
+        push_when_frame10(scores, number_of_shots, shots)
+        frames.push(Frame.new(shots[0], shots[1], shots[2]))
+        break
+      else
+        push_when_others(shot, shots, frames)
+        if shots.size == MAXIMUM_NUMBER_OF_SHOTS_IN_THE_FRAME
+          frames.push(Frame.new(shots[0], shots[1]))
+          shots.clear
+        end
+      end
+    end
+
+    frames
+  end
+
+  def push_when_frame10(scores, number_of_shots, shots)
+    scores[(number_of_shots - 1)..-1].each { |i| shots << Shot.new(i).score }
+  end
+
+  def push_when_others(shot, shots, frames)
+    if shots.size.zero? && (shot == 'X') # strike
+      frames.push(Frame.new(10, 0))
+    else
+      shots << Shot.new(shot).score
+    end
   end
 end
