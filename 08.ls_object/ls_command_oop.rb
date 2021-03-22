abe1073eb0e77279ad704e47f53a4ba8d3479524
ToshiroKoba <t-koba@ruby-dev.jp>
2021-03-22 11:24:09 +0900

    コマンドに関する処理をOptionCommandクラスに切り分け

--- 08.ls_object/ls_command_oop.rb
+++ 08.ls_object/ls_command_oop.rb
@@ -100,40 +100,42 @@ def output
   file_list = file_list.reverse if ARGV[0]&.include?('r')
   regex = (ARGV[0]&.include?('a') ? // : /^[^.]/)
   if ARGV[0]&.include?('l')
-    output_when_have_l_option(file_list, regex)
+    OptionCommand.output_when_have_l_option(file_list, regex)
   else
-    output_when_have_not_l_option(file_list, regex)
+    OptionCommand.output_when_have_not_l_option(file_list, regex)
   end
 end
 
-def output_when_have_not_l_option(file_list, regex)
-  result_list = push_file_that_matches_regular_expression(file_list, regex)
-  file_name_word_max_length = result_list.max_by(&:size).size
-  number = result_list.size.divmod(3)
-  number[0] += 1 if number[1] != 0
-  result_hash = result_list.group_by.with_index { |_file, i| i % number[0] }
-  result_hash.each_value do |array|
-    array.each do |file|
-      print file.ljust(file_name_word_max_length + 2)
+class OptionCommand
+  def self.output_when_have_not_l_option(file_list, regex)
+    result_list = push_file_that_matches_regular_expression(file_list, regex)
+    file_name_word_max_length = result_list.max_by(&:size).size
+    number = result_list.size.divmod(3)
+    number[0] += 1 if number[1] != 0
+    result_hash = result_list.group_by.with_index { |_file, i| i % number[0] }
+    result_hash.each_value do |array|
+      array.each do |file|
+        print file.ljust(file_name_word_max_length + 2)
+      end
+      print "\n"
     end
-    print "\n"
   end
-end
 
-def output_when_have_l_option(file_list, regex)
-  result_list = push_file_that_matches_regular_expression(file_list, regex)
-  word_max_length = Calculation.calculate_word_max_length(result_list)
-  puts "total #{Calculation.calculate_total(result_list)}"
-  result_list.each do |f|
-    file = File.new(f)
-    print file.output_permission
-    print file.output_nlink(word_max_length[:max_nlink])
-    print file.output_uid(word_max_length[:max_uid_name])
-    print file.output_gid(word_max_length[:max_gid_name])
-    print file.output_size(word_max_length[:max_size])
-    print file.output_month_and_day
-    print file.output_hour_and_minute
-    print file.output_name
+  def self.output_when_have_l_option(file_list, regex)
+    result_list = push_file_that_matches_regular_expression(file_list, regex)
+    word_max_length = Calculation.calculate_word_max_length(result_list)
+    puts "total #{Calculation.calculate_total(result_list)}"
+    result_list.each do |f|
+      file = File.new(f)
+      print file.output_permission
+      print file.output_nlink(word_max_length[:max_nlink])
+      print file.output_uid(word_max_length[:max_uid_name])
+      print file.output_gid(word_max_length[:max_gid_name])
+      print file.output_size(word_max_length[:max_size])
+      print file.output_month_and_day
+      print file.output_hour_and_minute
+      print file.output_name
+    end
   end
 end
 
