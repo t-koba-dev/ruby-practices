ae3fbe391b63eaca90aeb19368c34086212dfa06
ToshiroKoba <t-koba@ruby-dev.jp>
2021-03-26 11:33:16 +0900

    ファイルをオプション指定した時に、Fileクラスのインスタンスとして扱うよう変更

--- 09.wc_object/output.rb
+++ 09.wc_object/output.rb
@@ -13,23 +13,21 @@ class Output
   
   def self.output_without_stdin
     file_list = push_file_to_list
-    output_file_list = Calculation.calculate(file_list)
-    output_file_list.each_pair do |file_name, file|
-      file.each_value do |value|
-        print value.to_s.rjust(8)
-      end
-      print " #{file_name}\n"
+    file_list.each do |file|
+      print file.lines.to_s.rjust(8)
+      print file.words.to_s.rjust(8)
+      print file.bytes.to_s.rjust(8)
+      print " #{file.name}\n"
     end
-    output_total(file_list) if file_list.size >= 2
+    # output_total(file_list) if file_list.size >= 2
   end
   
   def self.push_file_to_list
     file_list = []
     Find.find('.') do |file|
       f = file.slice(2..-1)
-      file_list << f if ARGV.include?(f)
+      file_list << File.new(f) if ARGV.include?(f)
     end
-  
     exit if file_list.empty?
     file_list
   end
--- 09.wc_object/wc_command_oop.rb
+++ 09.wc_object/wc_command_oop.rb
@@ -2,6 +2,7 @@
 
 require 'find'
 require './09.wc_object/calculation'
+require './09.wc_object/file'
 require './09.wc_object/output'
 
 if $stdin.stat.mode.to_s(8)[0] == '1'
