983dbd53a5cec0c19e015fd7643399f2cbef052b
ToshiroKoba <t-koba@ruby-dev.jp>
2021-03-26 15:05:28 +0900

    不要な処理を削除

--- 09.wc_object/calculation.rb
+++ 09.wc_object/calculation.rb
@@ -1,18 +1,6 @@
 # frozen_string_literal: true
 
 class Calculation
-  def self.calculate(file_list)
-    output_file_list = Hash.new { |hash, key| hash[key] = {} }
-    file_list.each do |f|
-      output_file_list[:"#{f}"][:lines] = IO.readlines(f).size
-      unless ARGV.include?('-l')
-        output_file_list[:"#{f}"][:words] = IO.read(f).split(' ').size
-        output_file_list[:"#{f}"][:size] = IO.read(f).size
-      end
-    end
-    output_file_list
-  end
-
   def self.total(file_list)
     {
       lines: file_list.inject(0) { |result, file| result +file.lines },
